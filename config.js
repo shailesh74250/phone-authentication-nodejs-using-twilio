ee8303f71ab46f3cb8e7202690ab02306a8bb1bd
shailesh <familysoft20@gmail.com>
2020-11-06 10:38:27 +0530

    Remove config creds and tested login and verify both endpoint working perfectly

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	serviceID: '',
-	accountID: '',
-	outhToken: '',
-}
--- index.js
+++ index.js
@@ -2,42 +2,42 @@ const express = require('express');
 const app = express();
 
 const port = 4000;
-// const config = require('./config');
+const config = require('./config/index');
 
-// const client = require('twilio')(config.accountID, config.authToken);
+const client = require('twilio')(config.accountID, config.authToken);
 
-// app.get('/login', (req, res) => {
-// 	client
-// 		.verify
-// 		.services(config.serviceID)
-// 		.verifications
-// 		.create({
-// 			to: `+${req.query.phonenumber}`,
-// 			channel: req.query.channel
-// 		})
-// 		.then((data) => {
-// 			res.status(200).send(data);
-// 		}).catch((err) => {
-// 			res.status(500).send(err);
-// 		})
-// });
+app.get('/login', (req, res) => {
+	console.log(req.query.phonenumber);
+	console.log(req.query.channel);
+	client.verify
+		.services(config.serviceID)
+		.verifications.create({
+			to: `+${req.query.phonenumber}`,
+			channel: req.query.channel,
+		})
+		.then((data) => {
+			res.status(200).send(data);
+		})
+		.catch((err) => {
+			res.status(500).send(err);
+		});
+});
 
-// app.get('/verify', (req, res) => {
-// 	client
-// 		.verify
-// 		.services(config.serviceID)
-// 		.verificationChecks
-// 		.create({
-// 			to: `+${req.query.phonenumber}`,
-// 			code: req.query.code
-// 		})
-// 		.then((data) => {
-// 			res.status(200).send(data);
-// 		}).catch((err) => {
-// 			res.status(500).send(err);
-// 		})
-// })
+app.get('/verify', (req, res) => {
+	client.verify
+		.services(config.serviceID)
+		.verificationChecks.create({
+			to: `+${req.query.phonenumber}`,
+			code: req.query.code,
+		})
+		.then((data) => {
+			res.status(200).send(data);
+		})
+		.catch((err) => {
+			res.status(500).send(err);
+		});
+});
 
 app.listen(port, () => {
 	console.log(`Server is running at ${port}`);
-})
+});
